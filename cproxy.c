6529124bd1f54d81a792ea838f6f80352247e411
Emanuel Valente <emanuelvalente@gmail.com>
2015-05-04 20:35:10 -0700

    attempting to handle with different ip addresses

--- cproxy.c
+++ cproxy.c
@@ -63,111 +63,100 @@ void startCProxyServer(char *sproxyIPAddr) {
   /* connect to server */
   if (connect(socketFromSProxy,(struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
     error ("ERROR connecting");
+  fprintf(stderr, "Connected on %s:%d\n", sproxyIPAddr, SPROXY_PORT);
 
  /*at this point we can send some heartbeats*/
 
-  /*create a stream socket (TCP) -> to listen..*/
-  sockfd = socket (AF_INET, SOCK_STREAM, 0);
-  if(sockfd < 0)
-    error ("ERROR opening socket");
+  /*attempting */
+  while(1) {
+      /*create a stream socket (TCP) -> to listen..*/
+      sockfd = socket (AF_INET, SOCK_STREAM, 0);
+      if(sockfd < 0)
+          error ("ERROR opening socket");
 
-  memset (&serv_addr, 0, sizeof(serv_addr));
-  local_serv_addr.sin_family = AF_INET;
-  local_serv_addr.sin_addr.s_addr = INADDR_ANY;
-  local_serv_addr.sin_port = htons(LISTEN_PORT);
+      memset (&serv_addr, 0, sizeof(serv_addr));
+      local_serv_addr.sin_family = AF_INET;
+      local_serv_addr.sin_addr.s_addr = INADDR_ANY;
+      local_serv_addr.sin_port = htons(LISTEN_PORT);
 
-  if(bind(sockfd, (struct sockaddr *)&local_serv_addr, sizeof(struct sockaddr_in)) < 0)
-    error("ERROR on binding");
+      if(bind(sockfd, (struct sockaddr *)&local_serv_addr, sizeof(struct sockaddr_in)) < 0)
+          error("ERROR on binding");
 
-  /*Listen!*/
-  listen(sockfd, 5);
-  fprintf(stderr, "Listen on port %d\n", LISTEN_PORT);
-  clientlen = sizeof(client_addr);
 
-  /*accept first incoming connection on the pending queue,
+      /*Listen!*/
+      listen(sockfd, 5);
+      fprintf(stderr, "Listen on port %d\n", LISTEN_PORT);
+      clientlen = sizeof(client_addr);
+
+      /*accept first incoming connection on the pending queue,
   returns a new file descriptor*/
-  socketFromTelnetClient = accept(sockfd, (struct sockaddr *) &client_addr, &clientlen);
+      socketFromTelnetClient = accept(sockfd, (struct sockaddr *) &client_addr, &clientlen);
 
-  if(socketFromTelnetClient < 0)
-    error("ERROR on accept");
+      if(socketFromTelnetClient < 0)
+          error("ERROR on accept");
 
-  /*to initialize telnet connection on sproxy side*/
-  proxyPacket.header.type = NEW_CONNECTION_TYPE;
-  send(socketFromSProxy, &proxyPacket, sizeof(proxyPacket_t), 0);
+      /*to initialize telnet connection on sproxy side*/
+      proxyPacket.header.type = NEW_CONNECTION_TYPE;
+      send(socketFromSProxy, &proxyPacket, sizeof(proxyPacket_t), 0);
 
 
 
-  /*At this point we have two active sockets:
+      /*At this point we have two active sockets:
    * one is socketFromTelnetClient connected to the end user
    * and socketFromTelnetClient connected to the SProxy
    * */
-
-  /*testing... */
-  /*
-  strcpy(payload, "ma oi!");
-  bytes_sent = send(socketFromTelnetClient, payload, (sizeof(char) * strlen(payload) + 1), 0);
-  if (bytes_sent < 0)
-    error ("ERROR writing to socket");
-
-  memset(payload, 0, sizeof(char)* MAXPAYLOAD);
-  bytes_received = recv(socketFromTelnetClient, payload, sizeof(char) * MAXPAYLOAD, 0);
-
-  fprintf(stderr, "TELNET:\n%s\n", payload);
-  */
-  /*end of test*/
-
-  /*pooulating packet*/
-  heartBeatCount = 0;
+      while(1) {
+          /*clear the set ahead of time*/
+          FD_ZERO(&readfds);
+          FD_SET(socketFromTelnetClient, &readfds);
+          /*this socket is going to be "greater"*/
+          FD_SET(socketFromSProxy, &readfds);
 
 
-  while(1) {
-      /*clear the set ahead of time*/
-      FD_ZERO(&readfds);
-      FD_SET(socketFromTelnetClient, &readfds);
-      /*this socket is going to be "greater"*/
-      FD_SET(socketFromSProxy, &readfds);
+          /*setting our delay for the events*/
+          tv.tv_sec = 5;
+          tv.tv_usec = 500000;
 
+          /*param for select()*/
+          n = socketFromTelnetClient + 1;
 
-      /*setting our delay for the events*/
-      tv.tv_sec = 5;
-      tv.tv_usec = 500000;
+          ret = select(n, &readfds, NULL, NULL, &tv);
 
-      /*param for select()*/
-      n = socketFromTelnetClient + 1;
+          if(ret == -1)
+              fprintf(stderr, "Error in select()!\n");
+          else if(ret == 0)
+              fprintf(stderr, "Timeout occurred! No data after the specified time!\n");
+          else {
+              /*one of the both sockets has data to be received*/
+              if(FD_ISSET(socketFromTelnetClient, &readfds)) {
 
-      ret = select(n, &readfds, NULL, NULL, &tv);
-
-      if(ret == -1)
-          fprintf(stderr, "Error in select()!\n");
-      else if(ret == 0)
-          fprintf(stderr, "Timeout occurred! No data after the specified time!\n");
-      else {
-          /*one of the both sockets has data to be received*/
-          if(FD_ISSET(socketFromTelnetClient, &readfds)) {
-
-              bytes_received = recv(socketFromTelnetClient, proxyPacket.payload, sizeof(char) * MAXPAYLOAD, 0);
-              proxyPacket.header.type = APP_DATA_TYPE;
-              send(socketFromSProxy, &proxyPacket, sizeof(proxyHeader_t) + sizeof(char) * bytes_received, 0);
-          }
-
-          if(FD_ISSET(socketFromSProxy, &readfds)) {
-              bytes_received = recv(socketFromSProxy, &proxyPacket, sizeof(proxyPacket_t), 0);
-              if(proxyPacket.header.type == APP_DATA_TYPE)
-                  send(socketFromTelnetClient, proxyPacket.payload, sizeof(char) * bytes_received - sizeof(proxyHeader_t), 0);
-              if(proxyPacket.header.type == HEARTBEAT_TYPE) {
-                  heartBeatCount = proxyPacket.header.beatHeart++;
-                  send(socketFromSProxy, &proxyPacket, bytes_received, 0);
-                  fprintf(stderr,"cproxy received hearBeat.. sending %d\n", heartBeatCount);
+                  bytes_received = recv(socketFromTelnetClient, proxyPacket.payload, sizeof(char) * MAXPAYLOAD, 0);
+                  proxyPacket.header.type = APP_DATA_TYPE;
+                  send(socketFromSProxy, &proxyPacket, sizeof(proxyHeader_t) + sizeof(char) * bytes_received, 0);
               }
 
-              //fprintf(stderr,"telnet -> user %d bytes\n", bytes_received);
+              if(FD_ISSET(socketFromSProxy, &readfds)) {
+                  bytes_received = recv(socketFromSProxy, &proxyPacket, sizeof(proxyPacket_t), 0);
+                  if(proxyPacket.header.type == APP_DATA_TYPE)
+                      send(socketFromTelnetClient, proxyPacket.payload, sizeof(char) * bytes_received - sizeof(proxyHeader_t), 0);
+                  if(proxyPacket.header.type == HEARTBEAT_TYPE) {
+                      heartBeatCount = proxyPacket.header.beatHeart++;
+                      send(socketFromSProxy, &proxyPacket, bytes_received, 0);
+                      fprintf(stderr,"cproxy received hearBeat.. sending %d\n", heartBeatCount);
+                  }
+
+                  //fprintf(stderr,"telnet -> user %d bytes\n", bytes_received);
+              }
+
+
+              memset(&proxyPacket, 0, sizeof(proxyPacket_t));
+              bytes_received = 0;
+
           }
-
-
-          memset(&proxyPacket, 0, sizeof(proxyPacket_t));
-          bytes_received = 0;
-
       }
+
+  close(socketFromTelnetClient);
+
   }
 
 
